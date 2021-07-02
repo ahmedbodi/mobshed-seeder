6afae5faeb76809f51ee12c4f4ca49842a8df844
Ahmed Bodiwala <ahmed_bodi@msn.com>
2021-07-03 00:00:13 +0100

    [Conesnsus] Port to MobShed

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0xa5, 0x3b, 0x47, 0x3e };
 
 CMessageHeader::CMessageHeader()
 {
